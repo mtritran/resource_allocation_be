78c062c9b7c0bf57b7edced548a84fb093b47d73
Tran Manh Tri <tranmanhtri5104@gmail.com>
2026-07-15 13:32:16 +0700

    feat: 0.2 Initialize Spring Boot project with dependencies

---
+++ src/main/java/com/company/resourceallocation/ResourceAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.company.resourceallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceAllocationApplication.class, args);
+	}
+
+}
